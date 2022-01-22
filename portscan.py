d018b90d2503a5459f58a552ebae351cfdb7b04b
SandroSeidel <60406890+SandroSeidel@users.noreply.github.com>
2022-01-22 15:30:26 -0300

    better

--- portscan.py
+++ portscan.py
@@ -3,7 +3,6 @@
 import socket
 import sys 
 
-<<<<<<< HEAD
 #list off most used ports
 default_ports = [21, 22, 80, 8080, 443, 53, 3306, 3389]
 
@@ -26,8 +25,8 @@ for arg in sys.argv:
         ports = default_ports
     
     if arg == '-p':
-        a =1
-
+        a=1
+        
 
 for port in ports:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,21 +34,6 @@ for port in ports:
 
     try:
         response = s.connect_ex((sys.argv[1], port ))
-=======
-
-if len(sys.argv) < 2: 
-    print("Usage: portscan.py [IP] [ports - leave empty to listen to all ports]")    
-    sys.exit(1)
-
-default_ports = [21, 22, 80, 8080, 443, 53, 3306, 3389]
-
-for port in default_ports:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.5)
-
-    try:
-        response = client.connect_ex((sys.argv[1], port ))
->>>>>>> 62b7c4100eb2bfbd430d1e71468b0283c35f24b1
         if response == 0:
             print("Port: ", str(port), "open")
 
